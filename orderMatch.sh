fb5b519449df103b133ba72d39e71f03d7263270
caoshiwei <caoshiwei@lightinthebox.com>
2018-04-26 16:23:04 +0800

    测试æ测试数据库连接

--- orderMatch.sh
+++ orderMatch.sh
@@ -4,5 +4,6 @@ DBUSERNAME='order_bj'
 DBPASSWORD='pdpvYprwh69MN'
 DBHOST='172.16.0.96'
 
-mysql -h 172.16.0.96 -u order_bj -ppdpvYprwh69MN
+#mysql -h 172.16.0.96 -u order_bj -ppdpvYprwh69MN
 #mysql -h $DBHOST -u $DBUSERNAME -p$DBPASSWORD
+mysql -h 172.16.0.96 -u order_bj -pdpvYprwh69MN
