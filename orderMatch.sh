933101dae185d142d0bba817065e7eed024dca4d
caoshiwei <caoshiwei@lightinthebox.com>
2018-04-26 16:30:12 +0800

    更改数据库密码

--- orderMatch.sh
+++ orderMatch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #获取一定时间段内，已经配货完毕但是未发送库房的订单信息，如果订单数量超过阈值，则发送邮件给指定用户
 DBUSERNAME='order_bj'
-DBPASSWORD='pdpvYprwh69MN'
+DBPASSWORD='dpvYprwh69MN'
 DBHOST='172.16.0.96'
 
 mysql -h $DBHOST -u $DBUSERNAME -p$DBPASSWORD
